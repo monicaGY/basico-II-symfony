b89f35ae351a81710eb7d125e4074af3640a45e6
monicaGY <monicagarciayadaicela@gmail.com>
2023-07-13 16:39:06 +0200

    Formulario de tipo entidad

--- src/Controller/FormulariosController.php
+++ src/Controller/FormulariosController.php
@@ -9,6 +9,8 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
+use App\Entity\Persona;
+
 class FormulariosController extends AbstractController
 {
     #[Route('/formularios', name: 'form_inicio')]
@@ -56,5 +58,45 @@ class FormulariosController extends AbstractController
 
 
         return $this->render('formularios/simple.html.twig',compact('formulario'));
+    }
+
+
+    #[Route('/formularios/entity', name: 'form_entity')]
+    public function entity(Request $request): Response
+    {
+        $persona = new Persona();
+
+        $formulario = $this->createFormBuilder($persona)
+            ->add('nombre', TextType::class, ['label' => 'Nombre'])
+            ->add('correo', TextType::class, ['label' => 'Email'])
+            ->add('telefono', TextType::class, ['label' => 'Telefono'])
+            ->add('save', SubmitType::class,)
+            ->getForm();
+
+        
+
+        $submiteddToken= $request->request->get('token');
+        $formulario->handleRequest($request);
+        if($formulario->isSubmitted())
+        {
+            if($this->isCsrfTokenValid('generico',$submiteddToken))
+            {
+                $campos = $formulario->getData();
+                echo 'Nombre: '. $campos->getNombre();
+                echo '<br>Correo: '. $campos->getCorreo;
+                echo '<br>Teléfono: '. $campos->getTelefono;
+                die();
+            }
+            else
+            {
+
+                $this->addFlash('css','warning');
+                $this->addFlash('mensaje','Ocurrió un error inesperado');
+                return $this->redirectToRoute('form_entity');
+
+            }
+            
+        }
+        return $this->render('formularios/entity.html.twig',compact('formulario'));
     }
 }
